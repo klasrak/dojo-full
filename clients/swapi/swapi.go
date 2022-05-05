dabb3af221ba759386a6c03f44de4629ec492ad1
Danilo Augusto <danilo.augusto@mercadolivre.com>
2022-05-05 19:13:46 -0300

    add client logic

--- clients/swapi/swapi.go
+++ clients/swapi/swapi.go
@@ -1,6 +1,9 @@
 package swapi
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"swapi/models"
 )
@@ -17,18 +20,61 @@ type swapiClient struct {
 	baseURL string
 }
 
-func (sw *swapiClient) GetStarship(id int) (models.Starship, error) {
-	return models.Starship{}, nil
+func (sw *swapiClient) GetStarship(id int) (result models.Starship, err error) {
+	resource := fmt.Sprintf("/starships/%d/", id)
+	res, err := sw.client.Get(sw.baseURL + resource)
+
+	if err != nil {
+		return result, err
+	}
+
+	err = getBody(res, &result)
+
+	if err != nil {
+		return result, err
+	}
+
+	return result, err
 }
 
-func (sw *swapiClient) GetStarships() (models.Starships, error) {
-	return models.Starships{}, nil
+func (sw *swapiClient) GetStarships() (result models.Starships, err error) {
+	resource := "/starships/"
+	res, err := sw.client.Get(sw.baseURL + resource)
+
+	if err != nil {
+		return result, err
+	}
+
+	err = getBody(res, &result)
+
+	if err != nil {
+		return result, err
+	}
+
+	return result, err
 }
 
-func (sw *swapiClient) GetPeople(id int) (models.People, error) {
-	return models.People{}, nil
+func (sw *swapiClient) GetPeople(id int) (result models.People, err error) {
+	return result, err
 }
 
-func (sw *swapiClient) GetPeoples() (models.Peoples, error) {
-	return models.Peoples{}, nil
+func (sw *swapiClient) GetPeoples() (result models.Peoples, err error) {
+	return result, err
+}
+
+func getBody(res *http.Response, v interface{}) error {
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(body, v)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
