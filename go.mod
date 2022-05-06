04138c896199ac95829b5d5e68bfa18550e666fd
Danilo Augusto <danilo.augusto@mercadolivre.com>
2022-05-05 22:33:03 -0300

    refactor

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module swapi
 
 go 1.18
 
-require github.com/go-chi/chi/v5 v5.0.7
+require (
+	github.com/go-chi/chi/v5 v5.0.7
+	github.com/stretchr/testify v1.7.1
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
--- mockeable/mockeable.go
+++ mockeable/mockeable.go
@@ -1,6 +1,27 @@
 package mockeable
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type Mockeable interface {
+	GetFuncControls() []*CallsFuncControl
+	CleanUp()
+	Use()
+}
+
+func CleanUpAndAssertControls(t *testing.T, mock Mockeable) {
+	defer mock.CleanUp()
+	for _, control := range mock.GetFuncControls() {
+		if !control.IgnoreCallsAssertion {
+			assert.Equal(t, control.ExpectedCalls, control.funcCalls, fmt.Sprintf("expected calls for func %s does not match actual", control.funcName))
+		}
+	}
+}
 
 type CallsFuncControl struct {
 	funcName             string
